f3526616499561d05da46a9ad2dc101540c3bac5
Restyled.io <commits@restyled.io>
2023-08-18 04:08:40 +0000

    Restyled by black

--- db.py
+++ db.py
@@ -7,8 +7,9 @@ def insert(columns, table, values: list):
     with psycopg.connect(CONNECTION) as conn:
         with conn.cursor() as cur:
             columns = ", ".join(columns)
-            data = ", ".join([f"'{v}'" if isinstance(
-                v, str) else f"{v}" for v in values])
-            query = (f"INSERT INTO {table} ({columns}) VALUES ({data})")
+            data = ", ".join(
+                [f"'{v}'" if isinstance(v, str) else f"{v}" for v in values]
+            )
+            query = f"INSERT INTO {table} ({columns}) VALUES ({data})"
             cur.execute(query)
     return "Dato insertado"
