1db940b7e8823fd10a1aea21f6b3314dd9869aca
Restyled.io <commits@restyled.io>
2023-08-19 00:38:53 +0000

    Restyled by black

--- articulo.py
+++ articulo.py
@@ -1,39 +1,74 @@
 import db
 
 
-'''ARTICLE SECTION'''
+"""ARTICLE SECTION"""
 
 
 def get_all_articles():
-    columns = ["titulo", "conten", "autor_id",
-               "categoria_id", "pub_date", "last_update", "active"]
+    columns = [
+        "titulo",
+        "conten",
+        "autor_id",
+        "categoria_id",
+        "pub_date",
+        "last_update",
+        "active",
+    ]
     return db.select(columns, "articulo")
 
 
 def get_article(id):
-    columns = ["titulo", "conten", "autor_id",
-               "categoria_id", "pub_date", "last_update", "active"]
+    columns = [
+        "titulo",
+        "conten",
+        "autor_id",
+        "categoria_id",
+        "pub_date",
+        "last_update",
+        "active",
+    ]
     where = "art_id", "=", (id)
     return db.select(columns, "articulo", (where))
 
 
 def get_article_by_category(id):
-    columns = ["titulo", "conten", "autor_id",
-               "categoria_id", "pub_date", "last_update", "active"]
+    columns = [
+        "titulo",
+        "conten",
+        "autor_id",
+        "categoria_id",
+        "pub_date",
+        "last_update",
+        "active",
+    ]
     where = "categoria_id", "=", (id)
     return db.select(columns, "articulo", (where))
 
 
 def get_deactivated_articles(active):
-    columns = ["titulo", "conten", "autor_id",
-               "categoria_id", "pub_date", "last_update", "active"]
+    columns = [
+        "titulo",
+        "conten",
+        "autor_id",
+        "categoria_id",
+        "pub_date",
+        "last_update",
+        "active",
+    ]
     where = "active", "=", (active)
     return db.select(columns, "articulo", (where))
 
 
 def get_article_by_author(id):
-    columns = ["titulo", "conten", "autor_id",
-               "categoria_id", "pub_date", "last_update", "active"]
+    columns = [
+        "titulo",
+        "conten",
+        "autor_id",
+        "categoria_id",
+        "pub_date",
+        "last_update",
+        "active",
+    ]
     where = "autor_id", "=", (id)
     return db.select(columns, "articulo", (where))
 
@@ -54,8 +89,15 @@ def create_artticle(column_values):
     return db.insert(dict_vals, "articulo")
 
 
-dict_val = {"titulo": "Los drones", "conten": "Nuestro compañeros autónomos!!!.", "autor_id": 1,
-            "categoria_id": "3", "pub_date": "20230423 11:42:35.173", "last_update": "20230423 11:42:35.173", "active": 1}
+dict_val = {
+    "titulo": "Los drones",
+    "conten": "Nuestro compañeros autónomos!!!.",
+    "autor_id": 1,
+    "categoria_id": "3",
+    "pub_date": "20230423 11:42:35.173",
+    "last_update": "20230423 11:42:35.173",
+    "active": 1,
+}
 
 # print(get_all_articles())
 # print(get_article(2))
@@ -66,7 +108,7 @@ dict_val = {"titulo": "Los drones", "conten": "Nuestro compañeros autónomos!!!
 # print(modify_article(2, dict_val))
 print(create_artticle(dict_val))
 
-'''
+"""
 - Crear nuevos artículos
 - Modificar artículos existentes
 - Desactivar artículos
@@ -75,12 +117,12 @@ print(create_artticle(dict_val))
 - Obtener los artículos de una categoría
 - Obtener los artículos activos/publicados
 - Obtener los artículos de un autor
-'''
-'''
+"""
+"""
 titulo
 conten
 autor_id
 categoria_id
 pub_date
 last_update
-active'''
+active"""
