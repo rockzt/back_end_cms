54e87b0e795c30fdf0a8b38876a69e11dbe591c0
rodrigo zavala <rodrigoztrejo@gmail.com>
2023-08-21 19:57:40 -0600

    [modified] category.py several fixes, main.py minor fixes

--- category.py
+++ category.py
@@ -1,46 +1,19 @@
-import psycopg2
-from psycopg2 import sql
+import db
+#name_cate, descrip_cate
 
-# Conexión a la base de datos PostgreSQL
-conn = psycopg2.connect(
-    database="back_end_cms",
-    user="postgres",
-    password="mamita",
-    host="localhost",  # Cambia esto a la ubicación de tu base de datos
-    port="5432"  # Cambia esto al puerto de tu base de datos
-)
-
-# Crear un cursor para interactuar con la base de datos
-cursor = conn.cursor()
 
 # Función para crear una nueva categoría
-def create_category(name, description):
-    insert_query = sql.SQL("INSERT INTO category (name_cate, descrip_cate) VALUES ({}, {}) RETURNING category_id").format(
-        sql.Literal(name),
-        sql.Literal(description)
-    )
-    cursor.execute(insert_query)
-    category_id = cursor.fetchone()[0]
-    conn.commit()
-    return category_id
+def create_category(columns,values):
+  table="categoría"
+  return db.insert(columns, table, values)
 
 # Función para modificar los datos de una categoría
-def update_category(category_id, name, description):
-    update_query = sql.SQL("UPDATE category SET name_cate = {}, descrip_cate = {} WHERE category_id = {}").format(
-        sql.Literal(name),
-        sql.Literal(description),
-        sql.Literal(category_id)
-    )
-    cursor.execute(update_query)
-    conn.commit()
+def modify_categoria(id,columns, values ):
+    where = "categoria_id", "=", int(id)
+    return db.update(columns, "categoría", values ,(where))
 
 # Función para obtener una lista de categorías
-def get_all_categories():
-    select_query = sql.SQL("SELECT * FROM category")
-    cursor.execute(select_query)
-    return cursor.fetchall()
+def get_all_categorias():
+    columns = ["nom_cat", "descrip_cate"]
+    return db.select(columns, "categoría")
 
-# Cerrar la conexión cuando hayas terminado
-def close_connection():
-    cursor.close()
-    conn.close()
